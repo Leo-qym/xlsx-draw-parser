f5dc7ccdbaed31326120b246ea8e328ba336e259
Charles Allen <charles@courtHive.com>
2020-07-25 16:01:24 +0200

    translations

--- src/components/buttons/loadButton.jsx
+++ src/components/buttons/loadButton.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { useTranslation } from "react-i18next";
 
 import { CloudUpload } from '@material-ui/icons';
 import { IconButton, Tooltip } from '@material-ui/core/';
 import { dropModal } from 'components/dialogs/dragDropModal';
 import { loadSpreadsheet } from 'services/loadSpreadsheet';
 
-export function LoadButton(props) {
+export function LoadButton() {
+  const { t } = useTranslation();
   return (
-    <Tooltip title='Load' aria-label='load'>
+    <Tooltip title={t('Load')} aria-label={t('Load')}>
       <IconButton
         edge="start"
         color="inherit"
-        aria-label="load"
+        aria-label={t('Load')}
         onClick={()=>dropModal({callback: loadSpreadsheet})}
       >
         <CloudUpload />
